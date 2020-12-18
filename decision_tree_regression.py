59dd411ecc314f8a6fee66131f9f338a45f6c283
Kafando Rodrique <rdiuskaf@gmail.com>
2020-12-18 10:54:09 +0100

    Update decision_tree_regression.py

--- decision_tree_regression.py
+++ decision_tree_regression.py
@@ -1,7 +1,7 @@
 # Decision Tree Regression
 
 # Importing the libraries
-import numpy as np
+import numpy
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -19,7 +19,7 @@ regressor.fit(X, y)
 regressor.predict([[6.5]])
 
 # Visualising the Decision Tree Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = numpy.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
